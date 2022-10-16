caf0df7a008e500ed4d159b98f930e5c0bd2e57b
gm0308 <109142556+gm0308@users.noreply.github.com>
2022-10-16 10:47:27 +0900

    일단 만든 로그인 구현창

--- src/main/java/com/example/webmix/HelloController.java
+++ src/main/java/com/example/webmix/HelloController.java
@@ -1,12 +1,15 @@
 package com.example.webmix;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.PrintWriter;
 import java.sql.*;
 
 public class HelloController {
@@ -27,21 +30,6 @@ public class HelloController {
     }
 
     @FXML
-    private Button LoginBtn;
-
-    public void LoginScene() {
-        try {
-            Parent nextScene
-                    = FXMLLoader.load(getClass().getResource("main-view.fxml"));
-            Scene scene = new Scene(nextScene);
-            Stage primaryStage = (Stage) LoginBtn.getScene().getWindow();
-            primaryStage.setScene(scene);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @FXML
     private Label MainTxt;
 
     public void MainLabelScene() {
@@ -55,6 +43,8 @@ public class HelloController {
             e.printStackTrace();
         }
     }
+
+    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
 
     @FXML
     private TextField id;
@@ -79,11 +69,62 @@ public class HelloController {
             pstmt.setString(2, pw.getText());
             pstmt.setString(3, name.getText());
             pstmt.executeUpdate();
-            System.out.println("회원로그인이 되었습니다");
+            alert.setHeaderText("회원가입이 성공되셨습니다");
+            alert.setResizable(true);
+            alert.showAndWait();
 
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("같은 아이디가 있습니다");
+            alert.setHeaderText("같은 아이디가 있습니다");
+            alert.setResizable(true);
+            alert.showAndWait();
         }
     }
+
+//    로그인
+
+    @FXML
+    private Button LoginBtn;
+    public void LoginAction(ActionEvent event) {
+        String ID = this.id.getText();
+        String PW = this.pw.getText();
+        String Url = "jdbc:mysql://localhost:3306/webmix";
+        String bID = "root";
+        String dPW = "";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String sql = "";
+
+        try {
+            Class.forName("com.mysql.jdbc.users");
+            conn = DriverManager.getConnection(Url, bID, dPW);
+            sql = "select id,pw from webmix_users where id = ? and pw = ?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1,ID);
+            pstmt.setString(2,PW);
+            rs = pstmt.executeQuery();
+            if (rs.next() && rs.getString("id").equals(ID) && rs.getString("pw").equals(PW)) {
+                Parent mainPage = (Parent) FXMLLoader.load(this.getClass().getResource("main-view"));
+                StackPane root = (StackPane) this.LoginBtn.getScene().getRoot();
+                root.getChildren().add(mainPage);
+            }
+        } catch (Exception var25) {
+            var25.printStackTrace();
+        } finally {
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException ver24) {
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException var23) {
+                }
+            }
+        }
+    }
+
 }
