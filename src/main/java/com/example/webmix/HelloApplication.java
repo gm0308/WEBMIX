51f20afd76b0d0fcea33da838e7eb3e2d991a6db
domin03 <ckm7964@gmail.com>
2022-10-21 12:57:13 +0900

    물으표 넣어야함

--- src/main/java/com/example/webmix/HelloApplication.java
+++ src/main/java/com/example/webmix/HelloApplication.java
@@ -29,7 +29,7 @@ public class HelloApplication extends Application {
             public void handle(WindowEvent evt) {
                 Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
                 alert.setTitle("프로그램 종료");
-                alert.setHeaderText("잠깐! 시뮬레이션을 종료하시겠습니까?");
+                alert.setHeaderText("오늘 하루 WEBMIX에서 즐거웠나요");
                 alert.setContentText("확인 버튼 클릭 시 자동으로 로그아웃됩니다.");
                 Optional<ButtonType> result = alert.showAndWait();
                 if (result.get() == ButtonType.OK) {
